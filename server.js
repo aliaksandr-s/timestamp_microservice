748b20b5796708c228a21c61026bd380116faa67
aliaksandr-s <s-a-n-9@mail.ru>
2016-11-05 12:28:16 +0300

    additions

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 'use strict'
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const moment = require('moment');
+const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    console.log(req.url)
+    res.send('Hello World!')
 })
 
+app.use(function (req, res, next) {
+    const url = req.url.slice(1);
+    let date = convertStrnigToDate(url);
+    res.json(date);
+});
+
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
+    console.log('listening on port 8080!')
 })
+
+function convertStrnigToDate(url) {
+    let date = {
+        unix: null,
+        natural: null
+    }
+
+    if (moment.unix(url).isValid()) {
+        date.unix = +url;
+        date.natural = moment.unix(url).format("MMMM D, YYYY");
+    } else if (moment(url).isValid()) {
+        date.natural = moment(url).format("MMMM D, YYYY");
+        date.unix = +moment(url).format("X");
+    }
+
+    return date;
+}
